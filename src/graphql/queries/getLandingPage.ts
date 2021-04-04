39c42b8af7b4d9556559244b01411c18ad0c91f0
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-04-04 11:42:23 -0300

    Criando a primeira query

---
+++ src/graphql/queries/getLandingPage.ts
@@ -0,0 +1,11 @@
+const GET_LANGIND_PAGE = /* GraphQL */ `
+  query GET_LANDING_PAGE {
+    landingPage {
+      logo {
+        alternativeText
+        url
+      }
+    }
+  }
+`
+export default GET_LANGIND_PAGE
